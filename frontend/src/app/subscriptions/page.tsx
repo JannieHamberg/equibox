6e2d05e4c9cf2a5c043328bdb76aa6dc7d128430
Jannie Hamberg <jg311209@live.com>
2024-12-15 09:39:13 +0100

    Fixing vercel deploy error, empty  pages.

---
+++ frontend/src/app/subscriptions/page.tsx
@@ -0,0 +1,9 @@
+export default function Prenumerationer() {
+    return (
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Prenumerationer</h1>
+        <p>...</p>
+      </div>
+    );
+  }
+  
---
+++ frontend/src/app/userprofile/page.tsx
@@ -0,0 +1,9 @@
+export default function MittKonto() {
+    return (
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Mitt Konto</h1>
+        <p>Välkommen!</p>
+      </div>
+    );
+  }
+  
